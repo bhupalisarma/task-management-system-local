87c0f8885d61521f7ecbaeb85f65c4a270e82b44
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-06-18 16:05:24 +0530

    Server updated.

--- task-manager-backend/api/index.ts
+++ task-manager-backend/api/index.ts
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
